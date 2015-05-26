49126966fa6d0e47241f50e68bab351496914c84
Hack Reactor Students <accounts@hackreactor.com>
2015-05-25 20:03:38 -0700

    Finnished up to friendslist

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,9 +3,11 @@ var app = {
   server:'https://api.parse.com/1/classes/chatterbox'
 };
 
-app.rooms = {};
-app.users = {};
+app.chatRooms = {};
+app.currentUser;
+app.currentRoom;
 app.friends = {};
+app.numPost = 20;
 
 app.send = function(message) {
   console.log
@@ -53,16 +55,21 @@ app.clearMessages = function(){
 
 app.addMessage = function(message){
   $('#chats').append('<div><a class=\'username\' href=\"#\"> ' + message.username + "</a> : " + message.text + '</div>');
+
 };
 
 app.addRoom = function(roomname){
-  if (!app.rooms[roomname]) {
-    app.rooms[roomname] = true;
-    $('#roomSelect').append('<div><a href=\"#\">' + roomname + '</a></div>');
+  if (!app.chatRooms[roomname]) {
+    app.chatRooms[roomname] = true;
+    $('#roomSelect').append('<div><a class = \'chatRooms\' href=\"#\">' + roomname + '</a></div>')
+    // debugger;
+      .click(app.switchChatRoom);
+
   }
 };
 
 app.addFriend = function() {
+  console.log("adding friend");
   var username = this.text;
   if (!app.friends[username]) {
     app.friends[username] = true;
@@ -72,56 +79,82 @@ app.addFriend = function() {
 
 
 app.handleSubmit = function() {
-  var username = $.urlParam('username')
+  //var username = $.urlParam('username')
   var message = {
-    username: username,
+    username: app.currentUser,
     text:$('.message').val(),
-    chatroom: 'lobby'
+    roomname: app.currentRoom
   };
   app.send(message);
   app.fetch();
 };
 
 app.init = function() {
-  $('.username').click(app.addFriend);
+  app.currentUser = $.urlParam('username');
+  app.currentRoom = "Lobby";
+  app.addRoom(app.currentRoom);
+  $('.message').bind('keypress',function(event) {
+    if (event.keyCode === 13) {
+      $('.submit').click();
+    }
+  });
   $('.submit').click(function(){
     $('.submit').attr('disabled','disabled');
     app.handleSubmit();
   });
   $('.refresh').click(function() {
     $('.refresh').attr('disabled','disabled');
-    updateChat();
+    app.fetch();
   });
-
+  $('.currentRoom').text(app.currentRoom);
   setInterval(app.fetch, 50000);
 };
 
 var updateChat = function(data){
-    // debugger;
-  app.clearMessages();
-  var messageList = data.results.slice(0,10);
-  _.each(messageList, function (message){
-    message.username = validator.escape(message.username);
-    message.text = validator.escape(message.text);
-    message.roomname = validator.escape(message.roomname);
+  if (data === undefined) {
+    console.log('not receiving data');
+    return;
+  }
 
-    app.addMessage(message)
+  app.clearMessages();
+  var messageList = data.results.slice(0, app.numPost);
+  _.each(messageList, function (message){
+
+    // Temporary to see all messages
+    // if (app.currentRoom === 'Lobby' || message.roomname === app.currentRoom) {
+    if (message.roomname === app.currentRoom) {
+      message.username = validator.escape(message.username);
+      message.text = validator.escape(message.text);
+      message.roomname = validator.escape(message.roomname);
+
+      app.addMessage(message)
+    }
+
     app.addRoom(message.roomname);
+
   });
 
-}
+  $('.username').click(app.addFriend);
+
+};
+app.switchChatRoom= function () {
+  console.log("Switching room");
+  app.currentRoom = this.text;
+  $('.currentRoom').text(app.currentRoom);
+  $('.refresh').click();
+};
 
 $.urlParam = function(name){
     var results = new RegExp('[\?&amp;]' + name + '=([^&#]*)').exec(window.location.href);
     return results[1] || 0;
-}
+};
 
 var updateFriendsList = function(){
   $('.friends').empty();
-
   $('.friends').append('<ul></ul>');
 
   for(var i in app.friends) {
     $('.friends ul').append('<li class=\'friend\'>' + i + '</li>');
   }
-}
+
+};
