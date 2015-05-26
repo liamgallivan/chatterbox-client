1051b7810ce808ccdd9de167cbd2b787a85ffe0a
Hack Reactor Students <accounts@hackreactor.com>
2015-05-25 17:32:18 -0700

    Finished with basic app

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,8 +3,12 @@ var app = {
   server:'https://api.parse.com/1/classes/chatterbox'
 };
 
+app.rooms = {};
+app.users = {};
+app.friends = {};
 
 app.send = function(message) {
+  console.log
   $.ajax({
   // always use this url
   url: app.server,
@@ -12,10 +16,13 @@ app.send = function(message) {
   data: JSON.stringify(message),
   contentType: 'application/json',
   success: function (data) {
+    $('.submit').removeAttr('disabled');
+    $('.message').val('');
     console.log('chatterbox: Message sent');
     },
   error: function (data) {
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    $('.refresh').removeAttr('disabled');
     console.error('chatterbox: Failed to send message');
     }
   });
@@ -24,18 +31,20 @@ app.send = function(message) {
 app.fetch = function() {
   $.ajax({
   // always use this url
-  url: app.server,
-  type: 'GET',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
-  }
-});
+    url: app.server,
+    type: 'GET',
+    // data: data,
+    contentType: 'application/json',
+    success: function (data) {
+      updateChat(data);
+      updateFriendsList();
+      $('.refresh').removeAttr('disabled');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message');
+    }
+  });
 };
 
 app.clearMessages = function(){
@@ -43,25 +52,76 @@ app.clearMessages = function(){
 }
 
 app.addMessage = function(message){
-  $('#chats').prepend('<div class=\'.username\'> ' + message.username + ":" + message.text + '</div>')
-
+  $('#chats').append('<div><a class=\'username\' href=\"#\"> ' + message.username + "</a> : " + message.text + '</div>');
 };
 
 app.addRoom = function(roomname){
-  $('#roomSelect').append('<div>' + roomname + '</div>');
+  if (!app.rooms[roomname]) {
+    app.rooms[roomname] = true;
+    $('#roomSelect').append('<div><a href=\"#\">' + roomname + '</a></div>');
+  }
 };
 
-app.addFriend = function(username) {
-  console.log("Clicked");
+app.addFriend = function() {
+  var username = this.text;
+  if (!app.friends[username]) {
+    app.friends[username] = true;
+  };
+  updateFriendsList();
 };
 
 
 app.handleSubmit = function() {
+  var username = $.urlParam('username')
+  var message = {
+    username: username,
+    text:$('.message').val(),
+    chatroom: 'lobby'
+  };
+  app.send(message);
+  app.fetch();
 };
 
 app.init = function() {
-  $('.username').click(app.addFriend());
-  $('#send .submit').click(app.handleSubmit());
+  $('.username').click(app.addFriend);
+  $('.submit').click(function(){
+    $('.submit').attr('disabled','disabled');
+    app.handleSubmit();
+  });
+  $('.refresh').click(function() {
+    $('.refresh').attr('disabled','disabled');
+    updateChat();
+  });
+
+  setInterval(app.fetch, 50000);
 };
 
+var updateChat = function(data){
+    // debugger;
+  app.clearMessages();
+  var messageList = data.results.slice(0,10);
+  _.each(messageList, function (message){
+    message.username = validator.escape(message.username);
+    message.text = validator.escape(message.text);
+    message.roomname = validator.escape(message.roomname);
 
+    app.addMessage(message)
+    app.addRoom(message.roomname);
+  });
+
+}
+
+$.urlParam = function(name){
+    var results = new RegExp('[\?&amp;]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
+
+var updateFriendsList = function(){
+  $('.friends').empty();
+
+  $('.friends').append('<ul></ul>');
+
+  for(var i in app.friends) {
+    $('.friends ul').append('<li class=\'friend\'>' + i + '</li>');
+  }
+}
